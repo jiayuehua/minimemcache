cd45fe84bc2f66826c96899d169452439ba7d570
jiayuehua <jiayuehua@jiayuehua-THINK.(none)>
2013-11-26 02:36:52 +0800

    m

--- main.c
+++ main.c
@@ -3,6 +3,8 @@ int main(int argc,char *argv[])
 {
 printf(¡°hello.\n¡±);
 printf(¡°he was a student.\n¡±);
+printf(¡°he was born in finland.\n¡±);
+printf(¡°he was a man!!!!!!!!!!!!\n¡±);
 return 0;
 }
 
